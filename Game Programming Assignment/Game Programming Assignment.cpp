47822c67e12eaa882f1c1d6c20c8a0b5135ee1e4
AHitchen21 <alexhitchen1234@gmail.com>
2024-02-08 16:01:43 +0000

    Frist window

--- Game Programming Assignment/Game Programming Assignment.cpp
+++ Game Programming Assignment/Game Programming Assignment.cpp
@@ -2,10 +2,20 @@
 //
 
 #include <iostream>
+#include "SDL.h"
 
-int main()
+int main(int argc, char* argv[])
 {
-    std::cout << "Hello World!\n";
+    if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
+    {
+        return 1;
+    }
+
+    SDL_Window* window = SDL_CreateWindow("Lecture 2, Windowing!", 250, 250, 800, 600, SDL_WINDOW_SHOWN);
+
+    SDL_Delay(4200);
+
+    return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
